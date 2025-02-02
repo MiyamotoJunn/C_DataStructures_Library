5274a6e54b64c58ff21a9f5b1587a730ed92ffb2
MiyamotoJunn <helloolleh2153@gmail.com>
2025-02-02 20:25:12 +0900

    make DoublyLinkedList

---
+++ DoublyLinkedList/DLL.h
@@ -0,0 +1,24 @@
+#ifndef DLL_H
+#define DLL_H
+
+#include<stdlib.h>
+
+typedef int DLL_Type;
+
+typedef struct Node
+{
+	DLL_Type data;
+	struct Node *prev;
+	struct Node *next;
+}Node;
+
+Node	*DLL_Create_Node(DLL_Type data);
+void	DLL_Deleate_Node(Node *node);
+void	DLL_Append_Node(Node **p_head, Node *newNode);
+Node	*DLL_Get_Node_At(Node *head, int index);
+void	DLL_Remove_Node(Node **p_head, Node *node_to_remove);
+void	DLL_Insert_Node_After(Node *target_node, Node *after_node);
+int		DLL_Get_Count_Nodes(Node *head);
+
+
+#endif
