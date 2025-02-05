e7108a79adb85a260b7e250cd9ef47b79db43f34
MiyamotoJunn <helloolleh2153@gmail.com>
2025-02-05 17:27:49 +0900

    + ps 1158 in main.c

--- CircularLinkedList/main.c
+++ CircularLinkedList/main.c
@@ -1,57 +1,39 @@
 #include<stdio.h>
 #include "CDLL.h"
 
+// ps 1158
+
 int	main()
 {
-	int	N;
-	int	M;
-	int	*index_box;
+	int	n;
+	int	k;
 	int	i;
-
-	Node	*CDLL;
-	CDLL = NULL;
+	Node	*CDLL = NULL;
 	Node	*newNode;
-	int		totalCount = 0;
-	int		nextCount = 0;
-	int		prevCount = 0;
 
-	scanf("%d %d", &N, &M);
-	index_box = (int*)malloc(sizeof(int) * M);
-	if (index_box == NULL)
-		return (0);
+	scanf("%d %d", &n, &k);
 	i = 0;
-	while (i < M)
-	{
-		scanf("%d", &(index_box[i]));
-		i++;
-	}
-
-	i = 0;
-	while (i < N)
+	while (i < n)
 	{
 		newNode = CDLL_Create_Node(1 + i);
 		CDLL_Append_Node(&CDLL, newNode);
 		i++;
 	}
 	i = 0;
-	while (i < M)
+	printf("<");
+	while  (CDLL != NULL)
 	{
-		int data = index_box[i];
-		nextCount = CDLL_Get_Rindex_From_Data(CDLL, data);
-		prevCount = CDLL_Get_Lindex_From_Data(CDLL, data);
-		if (nextCount == prevCount)
-			totalCount += nextCount;
-		else if (nextCount < prevCount)
-			totalCount += nextCount;
+		Node	*node_to_delete;
+		node_to_delete = CDLL_Get_Node_From_Index(CDLL, k - 1);
+		if (CDLL->next != CDLL)
+		{
+			printf("%d, ", node_to_delete->data);
+		}
 		else
-			totalCount += prevCount;
-		Node *node_to_remove = CDLL_Get_Node_From_Index(CDLL, nextCount);
-		CDLL = node_to_remove;
-		CDLL_Remove_Node(&CDLL, node_to_remove);
-		i++;
+			printf("%d>", node_to_delete->data);
+		CDLL = node_to_delete;
+		CDLL_Remove_Node(&CDLL, node_to_delete);
 	}
-	printf("%d", totalCount);
-
 }
 	
 
