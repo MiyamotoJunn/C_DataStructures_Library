ccda3b90dd02cff7031f2a68b75070e141d39924
MiyamotoJunn <helloolleh2153@gmail.com>
2025-02-08 01:35:46 +0900

    + ps 1918 in main.c

--- LinkedListStack/main.c
+++ LinkedListStack/main.c
@@ -1,26 +1,114 @@
 #include "StringLLS.h"
 #include<stdio.h>
 
-int	main()
+//ps1918
+
+int	Is_High(char top, char tokken)
 {
-	LinkedListStack	*LLS;
+	int	top_score;
+	int	tokken_score;
+
+	if (top == '(')
+		return (0);
+	if (top == '*' || top == '/')
+		top_score = 2;
+	else
+		top_score = 1;
+	if (tokken == '*' || tokken == '/')
+		tokken_score = 2;
+	else
+		tokken_score = 1;
+	return (top_score >= tokken_score);
+}
+
+char	*infix_to_postfix(char *infix)
+{
+	int	i;
+	int	j;
+	char	*postfix;
+	char	*box;
+	LinkedListStack *LLS = NULL;
 	Node	*newNode;
 
-	LLS = NULL;
+	box = (char *)malloc(sizeof(char) + 1);
 	LLS_Create_Stack(&LLS);
-	newNode = LLS_Create_Node("ang");
-	LLS_Push(LLS, newNode);
-	newNode = LLS_Create_Node("gie");
-	LLS_Push(LLS, newNode);
-	newNode = LLS_Create_Node("moe");
-	LLS_Push(LLS, newNode);
-	newNode = LLS_Create_Node("zzy");
-	LLS_Push(LLS, newNode);
-	printf("Stack Size : %d\n", LLS_Get_Size(LLS));
-	printf("Top : %s\n", LLS_Top(LLS)->data);
-	printf("------------------------\n");
+	postfix = (char*)malloc(sizeof(char) * 101);
+	i = 0;
+	j = 0;
+	box[1] = '\0';
+	while (infix[i] != '\0')
+	{
+		if (infix[i] != '(' && infix[i] != ')' && infix[i] != '*'
+				&& infix[i] != '/' && infix[i] != '+' && infix[i] != '-')
+		{
+			postfix[j] = infix[i];
+			j++;
+		}
+		else
+		{
+			if (infix[i] == '(')
+			{
+				box[0] = infix[i];
+				newNode = LLS_Create_Node(box);
+				LLS_Push(LLS, newNode);
+			}
+			else if (infix[i] != ')')
+			{
+				postfix[j] = ' ';
+				j++;
+				if (LLS_Is_Empty(LLS))
+				{
+					box[0] = infix[i];
+					newNode = LLS_Create_Node(box);
+					LLS_Push(LLS, newNode);
+				}
+				else
+				{
+					while (Is_High(LLS->top->data[0], infix[i]))
+					{
+						postfix[j] = LLS_Pop(LLS)->data[0];
+						j++;
+						postfix[j] = ' ';
+						j++;
+						if (LLS_Is_Empty(LLS))
+							break;
+					}
+					box[0] = infix[i];
+					newNode = LLS_Create_Node(box);
+					LLS_Push(LLS, newNode);
+				}
+			}
+			else if (infix[i] == ')')
+			{
+				while (LLS_Top(LLS)->data[0] != '(')
+				{
+					postfix[j] = ' ';
+					j++;
+					postfix[j] = LLS_Pop(LLS)->data[0];
+					j++;
+				}
+				LLS_Pop(LLS);
+			}
+		}
+		i++;
+	}
 	while (! LLS_Is_Empty(LLS))
 	{
-		printf("%s\n", LLS_Pop(LLS)->data);
+		postfix[j] = ' ';
+		j++;
+		postfix[j] = LLS_Pop(LLS)->data[0];
+		j++;
 	}
+	postfix[j] = '\0';
+	return (postfix);
+}
+
+int	main()
+{
+	char	*infix = (char*)malloc(sizeof(char) * 101);
+	char	*postfix;
+	printf("Enter infix : ");
+	scanf("%s", infix);
+	postfix = infix_to_postfix(infix);
+	printf("%s", postfix);
 }
