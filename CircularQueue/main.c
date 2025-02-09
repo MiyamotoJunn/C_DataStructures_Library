facb680ff1d8f5b919eaf7d688af14b7da34039b
MiyamotoJunn <helloolleh2153@gmail.com>
2025-02-09 22:39:14 +0900

    + ps 2164 in main.c

--- CircularQueue/main.c
+++ CircularQueue/main.c
@@ -1,63 +1,28 @@
 #include "CQ.h"
 #include<string.h>
 
-//ps 10845
+//ps 2164
 
 int	main()
 {
 	int	n;
 	int	i;
-	char	*command;
 	CircularQueue *CQ = NULL;
-
 	scanf("%d", &n);
-	i = 0;
-	command = (char*)malloc(sizeof(char) * 6);
 	CQ_Create_Queue(&CQ, n);
-	while (i < n)
+	i = 1;
+	while (i <= n)
 	{
-		scanf("%s", command);
-		if (strcmp(command, "push") == 0)
-		{
-			int data;
-			scanf("%d", &data);
-			CQ_Enqueue(CQ, data);
-		}
-		else if (strcmp(command, "pop") == 0)
-		{
-			if (CQ_Is_Empty(CQ))
-				printf("-1\n");
-			else
-			{
-				printf("%d\n", CQ_Dequeue(CQ));
-			}
-		}
-		else if (strcmp(command, "size") == 0)
-		{
-			printf("%d\n", CQ_Get_Size(CQ));
-		}
-		else if (strcmp(command, "empty") == 0)
-		{
-			printf("%d\n", CQ_Is_Empty(CQ));
-		}
-		else if (strcmp(command, "front") == 0)
-		{
-			if (CQ_Is_Empty(CQ))
-				printf("-1\n");
-			else
-			{
-				printf("%d\n", CQ->nodes[CQ->front].data);
-			}
-		}
-		else if (strcmp(command, "back") == 0)
-		{
-			if (CQ_Is_Empty(CQ))
-				printf("-1\n");
-			else
-			{
-				printf("%d\n", CQ->nodes[CQ->rear - 1].data);
-			}
-		}
+		CQ_Enqueue(CQ, i);
 		i++;
 	}
+	int data;
+	while (CQ_Get_Size(CQ) != 1)
+	{
+		CQ_Dequeue(CQ);
+		data = CQ_Dequeue(CQ);
+		CQ_Enqueue(CQ, data);
+	}
+	printf("rast : %d", CQ_Dequeue(CQ));
+
 }
